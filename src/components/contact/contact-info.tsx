928fefdc5207d1a293a9c445aa9ae8091c47b5a7
Ganesh Kunwar <gkunwar09@gmail.com>
2024-11-29 00:25:44 +0545

    Updated contact form

--- src/components/contact/contact-info.tsx
+++ src/components/contact/contact-info.tsx
@@ -23,68 +23,17 @@ function ContactInfo() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div>
           <div>
-            <div className="mb-3 h-1 w-16 bg-success-main" />
-            <p className=" font-bold text-2xl uppercase">your inquiry</p>
+          <div className="mb-3 h-1 w-16 bg-success-main" />
+            <p className=" font-bold text-2xl uppercase">COMPANY INFORMATION</p>
           </div>
+          <div className="mb-3 h-1 w-16 bg-success-main" />
 
-          <p className="mt-4 text-sm leading-6 text-neutral-600">
-            Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx
-            zippy fowls. Few quips galvanized the mock jury box. Quick brown
-            dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves
-            quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was
-            gazed by MTV for luck. A wizard’s job is to vex chumps quickly in
-            fog. Watch &apos;Jeopardy!&apos;, Alex Trebek&apos; fun TV quiz
-            game. Woven silk pyjamas exchanged for blue quartz. Brawny gods
-            just.
+          <p className="text-sm text-neutral-600">
+              Tilottama-13, Patthardadha,
           </p>
-          <div className="mt-4">
-            <Form {...form}>
-              <FormField
-                control={form.control}
-                name="ds"
-                render={() => (
-                  <FormItem>
-                    <FormLabel> Your request</FormLabel>
-                    <FormControl>
-                      <Input />
-                    </FormControl>
-
-                    <div className="mt-2">
-                      <FormLabel>First Name</FormLabel>
-                      <FormControl>
-                        <Input />
-                      </FormControl>
-                    </div>
-
-                    <div className="mt-2">
-                      <FormLabel>Last Name</FormLabel>
-                      <FormControl>
-                        <Input />
-                      </FormControl>
-                    </div>
-
-                    <div className="mt-2">
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input />
-                      </FormControl>
-                    </div>
-
-                    <div className="mt-2">
-                      <FormLabel>Message</FormLabel>
-                      <FormControl>
-                        <Textarea />
-                      </FormControl>
-                    </div>
-                  </FormItem>
-                )}
-              />
-
-              <Button className="mt-3" variant={"success"} size={"lg"}>
-                Send
-              </Button>
-            </Form>
-          </div>
+          <p className="text-sm text-neutral-600">Rupandehi, Lumbini</p>
+          <p className="text-lg text-blue-900">9857029740</p>
+          <p className="text-sm text-neutral-600">lokraj@nepalets.com</p>
         </div>
 
         <div>
@@ -105,27 +54,6 @@ function ContactInfo() {
           </div>
 
           <div className="mt-2">
-            <div className="mb-3 h-1 w-16 bg-success-main" />
-            <p className=" font-bold text-2xl uppercase">company information</p>
-          </div>
-
-          <div className="mt-4">
-            <p className="text-sm text-neutral-600">
-              Tilottama-13, Patthardadha,
-            </p>
-            <p className="text-sm text-neutral-600">Rupandehi, Lumbini</p>
-            <p className="text-lg text-blue-900">9857029740</p>
-            <p className="text-sm text-neutral-600">lokraj@nepalets.com</p>
-          </div>
-
-          <div className="mt-4">
-            <p className="text-xl font-semibold text-neutral-900">
-              Download ETS Nepal Today!
-            </p>
-            <p className="text-neutral-600 text-sm">
-              Available on iOS and Android, ETS Nepal is your ultimate
-              transportation companion.
-            </p>
           </div>
         </div>
       </div>
