b1d8c80be33626d67f042ec2b12ece8ccc5c9dc4
Ganesh Kunwar <gkunwar09@gmail.com>
2025-08-29 09:53:12 +0545

    update about us page

--- src/components/about/who-we-are.tsx
+++ src/components/about/who-we-are.tsx
@@ -3,22 +3,11 @@ function WhoWeAre() {
     <section className="py-12">
       <div className="layout">
         <div className="flex flex-col items-center">
-          <p className="text-3xl font-semibold">Who we are</p>
-          <p className="text-neutral-400 text-xs">TAILORED LOGISTIC SERVICES</p>
-          <div className="mt-3 h-1 w-16 bg-success-main" />
-
-          <p className="text-2xl mt-4 text-center">
-            We are Trucking, logistics company operating in over 150 countries
-            worldwide, providing great shipping experience to our clients.
-          </p>
-
+          <p className="text-3xl font-semibold">E Transportation Service</p>
         </div>
 
         {/* Official Information Section */}
         <div className="mt-12 bg-neutral-50 p-6 rounded-2xl shadow-md">
-          <h3 className="text-xl font-semibold text-center mb-6">
-            Company Official Information
-          </h3>
 
           <div className="grid md:grid-cols-2 gap-6 text-sm">
             <div>
